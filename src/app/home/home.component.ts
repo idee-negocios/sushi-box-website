27810837cb3cd58ad6aea5c1b84cd176c155e3f1
Martin <chetinchog@yahoo.com>
2017-11-09 20:34:38 -0300

    Add h1 and responsive design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,24 +12,24 @@ export class HomeComponent implements OnInit {
 
   public carouselTileItems: Array<any>;
   public carouselTile: NgxCarousel;
-  
-   ngOnInit() {
-    this.carouselTileItems = ["s1","s2","s3","s4","s5","s6","s7","s8"];
-    
-       this.carouselTile = {
-         grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
-         slide: 1,
-         speed: 2000,
-         interval: 4000,
-         animation: 'lazy',
-         point: {
-           visible: true
-         },
-         load: 2,
-         touch: false,
-         easing: 'ease',
-         loop: true
-       }
-   }
+
+  ngOnInit() {
+    this.carouselTileItems = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'];
+
+    this.carouselTile = {
+      grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+      slide: 1,
+      speed: 2000,
+      interval: 4000,
+      animation: 'lazy',
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: false,
+      easing: 'ease',
+      loop: true
+    }
+  }
 
 }
