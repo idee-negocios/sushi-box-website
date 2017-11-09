bef04d27c784470c88f9143e27195fe97317a3b7
Ricardo Castellan <castellanra@gmail.com>
2017-11-09 13:51:29 -0300

    Page almost completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,15 +19,16 @@ export class HomeComponent implements OnInit {
        this.carouselTile = {
          grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
          slide: 1,
-         speed: 200,
-         interval: 500,
+         speed: 2000,
+         interval: 4000,
          animation: 'lazy',
          point: {
            visible: true
          },
          load: 2,
-         touch: true,
-         easing: 'ease'
+         touch: false,
+         easing: 'ease',
+         loop: true
        }
    }
 
